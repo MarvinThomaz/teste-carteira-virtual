9bfd1f2a0a609126d7d7ed81d1cde69eef4188b7
marvin_nascimento_accenture <marvin.do.nascimento@CPX-8A41XA9REBU>
2018-11-22 09:46:14 -0200

    update sketdh

--- client/sketch_nov22a.ino
+++ client/sketch_nov22a.ino
@@ -16,7 +16,19 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
  * Inicialização das portas.
  */
 void setup() {
-	Serial.begin(9600);
+  configure_rfid();
+  configure_valve();
+}
+
+/*
+ * Criação do looping de execução.
+ */
+void loop() {
+	read_rfid_card();
+}
+
+void configure_rfid() {
+  Serial.begin(9600);
 	
 	while (!Serial) {
 	  SPI.begin();
@@ -26,15 +38,13 @@ void setup() {
 	mfrc522.PCD_DumpVersionToSerial();
 	
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  
+}
+
+void configure_valve() {
   pinMode(7, OUTPUT);
 }
 
-/*
- * Criação do looping de execução.
- */
-void loop() {
-	
+void read_rfid_card() {
 	if (!mfrc522.PICC_IsNewCardPresent()) {
 		return;
 	}
@@ -45,5 +55,13 @@ void loop() {
 
 	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 	
+	open_valve();
+}
+
+void open_valve() {
   digitalWrite(7, HIGH);
+}
+
+void close_valve() {
+  digitalWrite(7, LOW);
 }
